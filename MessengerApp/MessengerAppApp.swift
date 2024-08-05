c3b5be19ac8febf011a5806190d5c8e04bffed75
jeoungsung12 <jeoungsung12@naver.com>
2024-08-05 19:43:35 +0900

    initial push

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by 정성윤 on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
