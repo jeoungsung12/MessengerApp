64fe1fd57c7db7d3b1eee581cb61891772a8d5bc
jeoungsung12 <jeoungsung12@naver.com>
2024-08-06 17:31:36 +0900

    init Setting

---
+++ MessengerApp/Extension/Color + Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Color + Extension.swift
+//  MessengerApp
+//
+//  Created by 정성윤 on 8/6/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let Color1 : Color = .init("Color 1")
+    static let Color2 : Color = .init("Color 2")
+    static let Color3 : Color = .init("Color 3")
+    static let Color4 : Color = .init("Color 4")
+    static let Color5 : Color = .init("Color 5")
+    static let Color6 : Color = .init("Color 6")
+    static let Color7 : Color = .init("Color 7")
+}
